b3506e55047a5ea2571010335f9399ac5d2f149b
Arthur Lee <aal1999arth@gmail.com>
2024-10-11 21:57:22 +0300

    Updated FILTER_Module.py

--- bioinflib_folder_1/FILTER_Module.py
+++ bioinflib_folder_1/FILTER_Module.py
@@ -1,4 +1,4 @@
-def find_gc_content(seq: str):
+def find_gc_content(seq: str) -> float:
     content = 0
     for nucl in seq:
         if nucl == 'G' or nucl == 'g' or nucl == 'C' or nucl == 'c':
@@ -6,6 +6,6 @@ def find_gc_content(seq: str):
     return (content / len(seq)) * 100
 
 
-def find_average_quality(quality_string: str):
+def find_average_quality(quality_string: str) -> float:
     total_quality = sum(ord(char) - 33 for char in quality_string)
     return total_quality / len(quality_string)
